c40002bd1dbd1ca49fc9b49b1c1869d8de45c53e
Slowmoney <frolenkov58@gmail.com>
2020-05-18 23:17:18 +0300

    d

--- vue.config.js
+++ vue.config.js
@@ -9,8 +9,8 @@ module.exports = {
       });
   },
 
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/' + process.env.CI_PROJECT_NAME + '/'
-    : '/'
-
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/dist/"
+      : "/"
 };
